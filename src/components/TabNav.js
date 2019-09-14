a4b2909245ec9b01fcfcbe2a8c4057b24fecf5f3
Kevin Grant <ConsciousK@yahoo.com>
2019-09-14 13:51:08 -0400

    Completed Navigation and calling Charaacters API and Rendering.

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Tab, Menu, Icon } from "semantic-ui-react";
-import { NavLink } from "react-router-dom";
-
+import { Tab } from "semantic-ui-react";
+import WelcomePage from './WelcomePage'
+import CharacterList from "./CharacterList";
 // TODO: Add missing menu/tabs/nav below
 
 // Review Semantic UI Component options for nav-like UI:
@@ -10,6 +10,22 @@ import { NavLink } from "react-router-dom";
 // https://react.semantic-ui.com/elements/button/
 // https://react.semantic-ui.com/collections/breadcrumb/
 
-export default function TabNav() {
+const panes = [
 
-};
+    { menuItem: { icon: 'home', content: 'Home', key: "home"},
+    render: () => <Tab.Pane><WelcomePage /></Tab.Pane> },
+
+    { menuItem: { icon: 'users', content: 'Characters', key: "char"},
+    render: () => <Tab.Pane><CharacterList /></Tab.Pane> },
+
+    { menuItem: { icon: 'map', content: 'Locations', key: "loc"},
+    render: () => <Tab.Pane>Locations Page</Tab.Pane> },
+
+    { menuItem: { icon: 'video camera', content: 'Episodes', key: "epi"},
+    render: () => <Tab.Pane>Episodes Page</Tab.Pane> },
+
+  ]
+  
+  const TabNav = () => <Tab panes={panes} />
+  
+  export default TabNav
